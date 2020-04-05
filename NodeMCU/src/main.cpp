9d7b671ac9114a249295518beeea47b6f49e5ac9
Alireza Ghodrati <apps.ghodrati1377@gmail.com>
2020-04-05 23:20:07 +0430

    Fixed Code

--- NodeMCU/src/main.cpp
+++ NodeMCU/src/main.cpp
@@ -5,14 +5,21 @@ const int trigPin = 2;  //D4
 const int echoPin = 0;  //D3
 
 
+const unsigned int BAUD_RATE=9600;
+
 // defines variables for HY-SRF05
 long duration;
 int distance;
 
+void ReadDataFromSensor();
+
+
 void setup() {
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output for HY-SRF05
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input for HY-SRF05
-  Serial.begin(9600); // Starts the serial communication for HY-SRF05
+  Serial.begin(BAUD_RATE);    // Starts the serial communication for HY-SRF05
+  Serial.println("setup called!");
+
+  pinMode(trigPin, OUTPUT);   // Sets the trigPin as an Output for HY-SRF05
+  pinMode(echoPin, INPUT);    // Sets the echoPin as an Input for HY-SRF05
  }
 
 void loop() {
@@ -21,22 +28,27 @@ void loop() {
 
 // read data from HY-SRF05 Sensor
 void ReadDataFromSensor(){
-  // Clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
+    // Clears the trigPin
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
 
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
 
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
 
-  // Calculating the distance
-  distance= duration*0.034/2;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  delay(2000);
+    // Calculating the distance
+    distance= duration*0.034/2;
+    // Prints the distance on the Serial Monitor
+   if(duration==0)
+       Serial.println("Warning: no pulse from sensor");
+    else{
+        Serial.print("distance to nearest object:");
+        Serial.print(distance);
+        Serial.println(" cm");
+    }
+    delay(100);
 }
