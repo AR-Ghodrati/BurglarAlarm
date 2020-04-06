4a36670380c96ef0cc460c269fa74fda2a81d318
Alireza Ghodrati <apps.ghodrati1377@gmail.com>
2020-04-06 11:44:49 +0430

    Added Moving Object Detect,Added ArduinoJson Lib

--- NodeMCU/src/main.cpp
+++ NodeMCU/src/main.cpp
@@ -1,11 +1,23 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include "ArduinoJson_ID64/ArduinoJson.h"
+
 
 // defines pins numbers for HY-SRF05
-const int trigPin = 2;  //D4
-const int echoPin = 0;  //D3
+#define trigPin  D4 
+#define echoPin  D3 
+#define ledPin   LED_BUILTIN
 
+#define ssid      "Burger Alarm System" // The name of the Wi-Fi network that will be created
+#define password  "1234658"             // The password required to connect to it, leave blank for an open
+#define HostName  "BugerAlarm.system"
 
 const unsigned int BAUD_RATE=9600;
+
+static bool EnableAlarm = false;
+static bool sensorEnabled = false;
+
+
 
 // defines variables for HY-SRF05
 long duration;
@@ -19,11 +31,25 @@ void setup() {
   Serial.println("setup called!");
 
   pinMode(trigPin, OUTPUT);   // Sets the trigPin as an Output for HY-SRF05
+  pinMode(ledPin, OUTPUT);    // Sets the LED as an Output
   pinMode(echoPin, INPUT);    // Sets the echoPin as an Input for HY-SRF05
- }
+
+  // first time , Wifi mode is Station
+  WiFi.softAP(ssid, password);          // Start the access point
+  Serial.print("Access Point \"");
+  Serial.print(ssid);
+  Serial.println("\" started");
+
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.softAPIP());     // Send the IP address of the ESP8266 to the computer
+  
+  WiFi.hostname(HostName);  // Set hostName for android application connect
+   
+}
 
 void loop() {
-   ReadDataFromSensor();
+   if(sensorEnabled) ReadDataFromSensor();
+  
 }
 
 // read data from HY-SRF05 Sensor
@@ -45,10 +71,19 @@ void ReadDataFromSensor(){
     // Prints the distance on the Serial Monitor
    if(duration==0)
        Serial.println("Warning: no pulse from sensor");
+    else if (distance >= 200)
+       Serial.println("Out of range");
     else{
+         if (distance <= 80 && distance >= 0) {
+            // object detected
+            digitalWrite(ledPin, HIGH);
+         } else {
+           //no object
+           digitalWrite(ledPin, LOW);
+         }
         Serial.print("distance to nearest object:");
         Serial.print(distance);
         Serial.println(" cm");
     }
-    delay(100);
+    delay(250);
 }
