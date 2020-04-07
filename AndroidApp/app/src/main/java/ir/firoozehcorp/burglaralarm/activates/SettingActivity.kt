ac910063df8cb3ae1f3d79431599a0d0b7d72e63
Alireza Ghodrati <apps.ghodrati1377@gmail.com>
2020-04-07 13:02:34 +0430

    Fixed SettingActivity

--- AndroidApp/app/src/main/java/ir/firoozehcorp/burglaralarm/activates/SettingActivity.kt
+++ AndroidApp/app/src/main/java/ir/firoozehcorp/burglaralarm/activates/SettingActivity.kt
@@ -6,10 +6,12 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import ir.firoozehcorp.burglaralarm.R
+import ir.firoozehcorp.burglaralarm.consts.Const
 import ir.firoozehcorp.burglaralarm.listeners.BoardApiListener
 import ir.firoozehcorp.burglaralarm.models.ApiResponse
 import ir.firoozehcorp.burglaralarm.models.board.BoardConfig
 import ir.firoozehcorp.burglaralarm.utils.ApiRequestUtil
+import ir.firoozehcorp.burglaralarm.utils.NetworkUtil
 import ir.firoozehcorp.burglaralarm.utils.StorageUtil
 import kotlinx.android.synthetic.main.activity_setting.*
 
@@ -25,60 +27,66 @@ class SettingActivity : AppCompatActivity() {
         fromIntro = intent.getBooleanExtra("fromIntro", false)
 
         submit.setOnClickListener {
-            try {
-                val _ssid = ssid.text.toString().trim()
-                val pass = password.text.toString().trim()
-                val _distance = distance.text.toString().trim().toInt()
+            if (checkConnectionToBoard()) {
+                try {
+                    val _ssid = ssid.text.toString().trim()
+                    val pass = password.text.toString().trim()
+                    val _distance = distance.text.toString().trim().toInt()
 
-                if (_ssid.isNotEmpty() && pass.isNotEmpty() && _distance < 120) {
-                    submit.visibility = View.GONE
-                    loading.visibility = View.VISIBLE
+                    if (_ssid.isNotEmpty() && pass.isNotEmpty() && _distance < 120) {
+                        submit.visibility = View.GONE
+                        loading.visibility = View.VISIBLE
 
-                    canBack = false
-                    ApiRequestUtil.sendBoardConfig(
-                        this,
-                        BoardConfig(
-                            StorageUtil.getDeviceID(this),
-                            _ssid,
-                            pass,
-                            _distance
-                        ),
-                        object : BoardApiListener {
-                            override fun onResponse(res: ApiResponse) {
-                                submit.visibility = View.VISIBLE
-                                loading.visibility = View.GONE
+                        canBack = false
+                        ApiRequestUtil.sendBoardConfig(
+                            this,
+                            BoardConfig(
+                                StorageUtil.getDeviceID(this),
+                                _ssid,
+                                pass,
+                                _distance
+                            ),
+                            object : BoardApiListener {
+                                override fun onResponse(res: ApiResponse) {
+                                    submit.visibility = View.VISIBLE
+                                    loading.visibility = View.GONE
 
-                                Toast.makeText(
-                                    this@SettingActivity,
-                                    "Data Saved Successfully",
-                                    Toast.LENGTH_SHORT
-                                ).show()
+                                    Toast.makeText(
+                                        this@SettingActivity,
+                                        "Data Saved Successfully",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
 
-                                StorageUtil.setSettingsOk(true, this@SettingActivity)
-                                canBack = true
-                            }
+                                    StorageUtil.setSettingsOk(true, this@SettingActivity)
+                                    canBack = true
+                                }
 
-                            override fun onError(errMsg: String) {
-                                submit.visibility = View.VISIBLE
-                                loading.visibility = View.GONE
+                                override fun onError(errMsg: String) {
+                                    submit.visibility = View.VISIBLE
+                                    loading.visibility = View.GONE
 
-                                Toast.makeText(
-                                    this@SettingActivity,
-                                    "Error happened : $errMsg",
-                                    Toast.LENGTH_LONG
-                                ).show()
-                                canBack = false
-                            }
-                        })
-                } else {
-                    if (_ssid.isEmpty()) ssid.error = "SSID cant be empty!"
-                    if (pass.isEmpty()) password.error = "SSID cant be empty!"
-                    if (_distance >= 120) distance.error = "distance cant be grater than 120 cm!"
+                                    Toast.makeText(
+                                        this@SettingActivity,
+                                        "Error happened : $errMsg",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                    canBack = false
+                                }
+                            })
+                    } else {
+                        if (_ssid.isEmpty()) ssid.error = "SSID cant be empty!"
+                        if (pass.isEmpty()) password.error = "SSID cant be empty!"
+                        if (_distance >= 120) distance.error =
+                            "distance cant be grater than 120 cm!"
+                    }
+                } catch (e: Exception) {
+                    Toast.makeText(this, "Error happened : " + e.message, Toast.LENGTH_LONG).show()
                 }
-            } catch (e: Exception) {
-                Toast.makeText(this, "Error happened : " + e.message, Toast.LENGTH_LONG).show()
-            }
-
+            } else Toast.makeText(
+                this,
+                "Must Connect to WiFi with Name \"${Const.SSID}\"",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
@@ -87,6 +95,10 @@ class SettingActivity : AppCompatActivity() {
             if (fromIntro) startActivity(Intent(this, IntroActivity::class.java))
             super.onBackPressed()
         }
+    }
+
+    private fun checkConnectionToBoard(): Boolean {
+        return NetworkUtil.getCurrentSsid(this) == Const.SSID
     }
 
 }
