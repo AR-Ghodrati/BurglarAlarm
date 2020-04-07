e7383a85fbb849c075303b3f82e9da3cfc09ff75
Alireza Ghodrati <apps.ghodrati1377@gmail.com>
2020-04-07 19:05:04 +0430

    Fix NotificationUtil

--- AndroidApp/app/src/main/java/ir/firoozehcorp/burglaralarm/utils/NotificationUtil.kt
+++ AndroidApp/app/src/main/java/ir/firoozehcorp/burglaralarm/utils/NotificationUtil.kt
@@ -4,6 +4,7 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.media.AudioAttributes
 import android.net.Uri
 import android.os.Build
 import androidx.core.app.NotificationCompat
@@ -15,7 +16,8 @@ object NotificationUtil {
     private var mManager: NotificationManager? = null
     private const val ANDROID_CHANNEL_ID = "ir.firoozehcorp.burglaralarm.ANDROID"
     private const val ANDROID_CHANNEL_NAME = "Burglar Alarm Notification"
-    private const val NOTI_ID = 12121213
+    private const val NOTI_IDEL_ID = 12121213
+    private const val NOTI_ALARM_ID = 12121214
 
 
     fun createNotification(context: Context, isEnable: Boolean) {
@@ -28,7 +30,17 @@ object NotificationUtil {
                 ANDROID_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT
             )
 
+            // Creating an Audio Attribute
+            val audioAttributes = AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_ALARM)
+                .build()
+
             channel.enableLights(true)
+            channel.setSound(
+                Uri.parse("android.resource://" + context.packageName.toString() + "/raw/alarm.mp3"),
+                audioAttributes
+            )
             channel.importance = NotificationManager.IMPORTANCE_HIGH
             channel.enableVibration(true)
             channel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
@@ -50,18 +62,31 @@ object NotificationUtil {
             builder.setContentText("Danger : Activity Detected")
             noti = builder.build()
 
-            noti.sound = Uri.parse(
-                "android.resource://"
-                        + context.packageName + "/" + R.raw.alarm
-            )
+            noti.sound =
+                Uri.parse("android.resource://" + context.packageName.toString() + "/" + R.raw.alarm)
+
+            noti.defaults =
+                Notification.DEFAULT_LIGHTS or Notification.DEFAULT_VIBRATE
+
+
+            disableNotification(context, true)
+            mManager?.notify(NOTI_ALARM_ID, noti)
         } else {
             builder.setContentText("Burglar Alarm Activated")
             noti = builder.build()
             noti.defaults = noti.defaults or Notification.DEFAULT_SOUND
             noti.defaults = noti.defaults or Notification.DEFAULT_VIBRATE
+
+            disableNotification(context, false)
+            mManager?.notify(NOTI_IDEL_ID, noti)
         }
 
-        mManager?.notify(NOTI_ID, noti)
+    }
+
+    fun disableNotification(context: Context, isAlarm: Boolean) {
+        mManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (isAlarm) mManager?.cancel(NOTI_IDEL_ID)
+        else mManager?.cancel(NOTI_ALARM_ID)
     }
 
     fun disableNotification(context: Context) {
